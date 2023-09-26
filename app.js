7b17021678ed681c5d3b7c9b774c317eb371fb7d
Ingrid Vanetta <ingridvanetta@gmail.com>
2023-09-26 11:40:44 -0300

    pantalla4

---
+++ app.js
@@ -0,0 +1,9 @@
+let signInButton = document.getElementById("signIn");
+let registro = document.getElementById("registro")
+signInButton.addEventListener("click", function() {
+    window.location.href = "pantalla_3.html";
+});
+registroButton.addEventListener("click",function(){
+    window.location.href = "pantalla_3.html";
+})
+
